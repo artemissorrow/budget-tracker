fa789347e1afcaab5576695f8427c42ef0acaa6b
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-04-26 20:46:27 -0400

    registered service worker, fixed file paths in manifest

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/manifest.webmanifest",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+]
+
