63a85135d3bdb0fbf9e2646faa532c2bdbe59133
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-04-28 17:37:33 -0400

    fixed service worker fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,12 +18,6 @@ self.addEventListener('install', (event) => {
     })
   )
 
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache')
-      return cache.add("/api/transaction")
-    })
-  )
 
   self.skipWaiting();
 })
@@ -45,14 +39,32 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Cache hit - return response
-      if (response) {
-        return response
-      }
-      return fetch(event.request)
-    })
-  )
-})
+self.addEventListener("fetch", (evt) => {
+  
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;}
+
+    evt.respondWith(
+      caches.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      })
+    );
+  });
